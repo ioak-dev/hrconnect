14eefe948f2fd32365197c2081096b51cb4df3dc
Shashank Mittal <shashank.mittal@westernacher.com>
2020-08-19 15:43:44 +0530

    Adding summary wizard

--- src/app/travel/wizard/submit/submit.component.ts
+++ src/app/travel/wizard/submit/submit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProject} from '../../models/project';
+import {ITravel} from '../../models/travel';
 
 @Component({
   selector: 'app-submit',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit.component.css']
 })
 export class SubmitComponent implements OnInit {
+  projectDetails: IProject[] = [];
+  travelType: string;
+  cabDetails: ITravel[];
+  trainDetails: ITravel[];
+  busDetails: ITravel[];
+  flightDetails: ITravel[];
+  hotelDetails = [];
+  visaDetail = [];
+  insuranceDetails = [];
 
-  constructor() { }
+  constructor() {
+    this.projectDetails = JSON.parse(sessionStorage.getItem('ProjectDetails'));
+
+  }
 
   ngOnInit() {
+    this.loadDaataFromStore();
+  }
+
+  loadDaataFromStore() {
+    this.projectDetails = JSON.parse(sessionStorage.getItem('ProjectDetails'));
+    this.travelType = sessionStorage.getItem('TravelType');
+    this.cabDetails = JSON.parse(sessionStorage.getItem('CabDetails'));
+    this.trainDetails = JSON.parse(sessionStorage.getItem('TrainDetails'));
+    this.busDetails = JSON.parse(sessionStorage.getItem('BusDetails'));
+    this.flightDetails = JSON.parse(sessionStorage.getItem('FlightDetails'));
+    this.hotelDetails = JSON.parse(sessionStorage.getItem('HotelDetails'));
+    this.visaDetail = JSON.parse(sessionStorage.getItem('VisaDetails'));
+    this.insuranceDetails = JSON.parse(sessionStorage.getItem('InsuranceDetails'));
+    console.log(this.projectDetails);
   }
 
 }
