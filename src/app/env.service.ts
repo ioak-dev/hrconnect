5a1feb409eb4effa4b399f2180fa6eb9c5ef2d99
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2020-09-13 21:59:47 +0530

    deployment changes

--- src/app/env.service.ts
+++ src/app/env.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  public backendUrl = 'http://localhost:8080';
-  public webUrl = 'http://localhost:4200';
+  // public backendUrl = 'http://localhost:8080';
+  public backendUrl = 'https://hrconnect-service.herokuapp.com/';
 
   constructor() { }
 }
